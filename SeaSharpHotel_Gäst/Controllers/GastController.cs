7aea20ddd28f5ca76923efa01138480537dfa476
Pedro Silva Esteves <pesi0006@student.hv.se>
2021-02-15 10:05:54 +0100

    Inloggning

---
+++ SeaSharpHotel_Gäst/Controllers/GastController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeaSharpHotel_Gäst.Controllers
+{
+    [Authorize]
+    public class GastController : Controller
+    {        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<ActionResult> SignOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
