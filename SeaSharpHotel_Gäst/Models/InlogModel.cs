3b3db1b4a6711bf7bd04d3b81777225aa0470ac2
PedroMSE <76793915+PedroMSE@users.noreply.github.com>
2021-02-15 15:07:32 +0100

    Add files via upload

---
+++ SeaSharpHotel_Gäst/Models/InlogModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeaSharpHotel_Gäst.Models
+{
+    public class InlogModel
+    {
+        public string GastMejl { get; set; }
+        public string Losenord { get; set; }
+    }
+}
