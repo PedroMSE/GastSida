407545cc897c20066bfde4ab132ef194c5d0a3e8
PedroMSE <76793915+PedroMSE@users.noreply.github.com>
2021-02-15 15:24:19 +0100

    Add files via upload

--- SeaSharpHotel_Gäst/Startup.cs
+++ SeaSharpHotel_Gäst/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,10 @@ namespace SeaSharpHotel_Gäst
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                    .AddCookie(options => { options.LoginPath = "/Inlog/Index/"; });
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +44,8 @@ namespace SeaSharpHotel_Gäst
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
