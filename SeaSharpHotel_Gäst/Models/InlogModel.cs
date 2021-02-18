b835a036f721321e2548d33057b54d616ec3e80a
Pedro Silva Esteves <pesi0006@student.hv.se>
2021-02-18 14:52:24 +0100

    Inlog model ändringar

--- SeaSharpHotel_Gäst/Models/InlogModel.cs
+++ SeaSharpHotel_Gäst/Models/InlogModel.cs
@@ -2,12 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SeaSharpHotel_Gäst.Models
 {
     public class InlogModel
     {
+        [Required(ErrorMessage = "Enter your email")]
+        [Display(Name ="Email")]
+
         public string GastMejl { get; set; }
+
+        [Required(ErrorMessage = "Enter your Password")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+
         public string Losenord { get; set; }
     }
 }
