30f23b8716c7894128b7b9bb3d070fc36e5475c6
Pedro Silva Esteves <pesi0006@student.hv.se>
2021-02-22 10:12:05 +0100

    Ändringar

--- SeaSharpHotel_Gäst/Controllers/AdminController.cs
+++ SeaSharpHotel_Gäst/Controllers/AdminController.cs
@@ -1,19 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SeaSharpHotel_Gäst.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace SeaSharpHotel_Gäst.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: AdminController
-        public ActionResult Index()
+        // GET: AdminController        
+        public async Task<IActionResult> Index()
         {
-            
-            return View();
+            List<Guest> guests = new List<Guest>();
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetAsync("http://193.10.202.78/GuestAPI/api/Guest");
+            string jsonresponse = await response.Content.ReadAsStringAsync();
+            guests = JsonConvert.DeserializeObject<List<Guest>>(jsonresponse);
+
+            return View(guests);
         }
 
         // GET: AdminController/Details/5
